fcc9edb80fc9a1dca133ae682470fc71a5037927
Unknown <nero.kiryu@gmail.com>
2018-05-30 11:14:31 +0200

    update

--- src/main/java/org/utbm/gameoflife/exemple/FenetreApplication.java
+++ src/main/java/org/utbm/gameoflife/exemple/FenetreApplication.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.utbm.gameoflife;
+package org.utbm.gameoflife.exemple;
 
 /**
  *
